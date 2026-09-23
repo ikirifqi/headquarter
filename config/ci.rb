c0c8e0f7916979f25a9e1c039c9a839b7997b997
Rifqi Fauzi <7533952+ikirifqi@users.noreply.github.com>
2026-09-23 23:45:34 +0700

    ci: configure GitHub Actions workflow for code checks and testing

--- config/ci.rb
+++ config/ci.rb
@@ -4,20 +4,18 @@ CI.run do
   step 'Setup', 'bin/setup --skip-server'
 
   step 'Style: Ruby', 'bin/rubocop'
+  step 'Style: ERB', 'bundle exec erb_lint --lint-all'
+  step 'Style: Frontend', 'npm run check'
 
   step 'Security: Gem audit', 'bin/bundler-audit'
-  step 'Security: Brakeman code analysis', 'bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error'
-  step 'Tests: Rails', 'bin/rails test'
-  step 'Tests: Seeds', 'env RAILS_ENV=test bin/rails db:seed:replant'
-
-  # Optional: Run system tests
-  # step "Tests: System", "bin/rails test:system"
+  step 'Security: Brakeman code analysis', 'bin/brakeman', '--quiet', '--no-pager', '--exit-on-warn', '--exit-on-error'
+  step 'Tests: Rails', 'bin/rails', 'test:all'
 
   # Optional: set a green GitHub commit status to unblock PR merge.
   # Requires the `gh` CLI and `gh extension install basecamp/gh-signoff`.
   # if success?
-  #   step "Signoff: All systems go. Ready for merge and deploy.", "gh signoff"
+  #   step 'Signoff: All systems go. Ready for merge and deploy.', 'gh signoff'
   # else
-  #   failure "Signoff: CI failed. Do not merge or deploy.", "Fix the issues and try again."
+  #   failure 'Signoff: CI failed. Do not merge or deploy.', 'Fix the issues and try again.'
   # end
 end
